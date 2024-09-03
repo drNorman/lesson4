932c380934bfb0b5ecf4abedd437c7909774d135
drNorman <71461772+drNorman@users.noreply.github.com>
2024-09-03 10:59:13 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+#1st program
+my_string = input('Введите произвольный текст.')
+print(len(my_string))
+
+#2st program
+print(my_string.upper())
+print(my_string.lower())
+my_string = my_string.replace(' ','')
+print(my_string[0])
+print(my_string[-1])
